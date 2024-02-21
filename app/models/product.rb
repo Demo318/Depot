cfc43b52a3d811f69ba15b13d49bb4382b60cbf0
Devin Mork <devin.mork@gmail.com>
2024-02-21 16:00:48 -0600

    Complete Iteration B1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+ 
+
+  # allow_blank: means we're not checking for presence twice,
+  # because it's already checked by the validates(presence: true)
+  # call above.
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image.'
+  }
+
 end
