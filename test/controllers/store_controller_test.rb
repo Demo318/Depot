d03411d95482624410c1e1e3035b141f8f89cecd
Devin Mork <devin.mork@gmail.com>
2024-02-26 08:33:15 -0600

    Complete Iteration C4

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,13 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select 'main ul li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
+
+    # Confirms there's a div that's displaying a price: Starts with a $,
+    # then any number of commas and digits, then a decimal point, then
+    # two digits.
+    assert_select 'div', /\$[, \d]+\.\d\d/
   end
 end
